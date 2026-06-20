3369a08af25a8f6a7d488a017b16ec7852d8f445
zx9qv8n4rpl7t6k2m1y <s.amogus.drip@gmail.com>
2026-06-20 17:51:42 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import App from 'https://cdn.jsdelivr.net/gh/zx9qv8n4rpl7t6k2m1y/ClearView@main/src/App.tsx';
+import 'https://cdn.jsdelivr.net/gh/zx9qv8n4rpl7t6k2m1y/ClearView@main/src/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
